0c666f5e9692eee9e22a06f4aadb96b7441785b8
Arcus-md <arcus1729@proton.me>
2026-02-28 15:49:34 +0530

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
-export const runtime = 'edge';
-import { NextResponse } from "next/server";
 
+import { NextResponse } from "next/server";
+export const runtime = 'edge';
 export async function GET() {
   return NextResponse.json({ message: "Hello, world!" });
 }
